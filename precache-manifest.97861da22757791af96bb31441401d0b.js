211d88b92827f40f678c5171133c441ad2642f99
Tania2101 <taniag21012001@gmail.com>
2020-06-21 17:56:46 +0530

    Updates

---
+++ precache-manifest.97861da22757791af96bb31441401d0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514c92833d0685b760e9bcf998267478",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "89da544b690f9c37318e",
+    "url": "/react-deploy/static/css/main.98e37881.chunk.css"
+  },
+  {
+    "revision": "b5af0404be7ea3d8d0f3",
+    "url": "/react-deploy/static/js/2.ebbf318f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.ebbf318f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89da544b690f9c37318e",
+    "url": "/react-deploy/static/js/main.3cf07246.chunk.js"
+  },
+  {
+    "revision": "ca763e7e278b8a588509",
+    "url": "/react-deploy/static/js/runtime-main.5fe2e597.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
